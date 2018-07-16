5e8dae43feadade1c5320bd731b0d6845a9884ec
schwarro <rob.ap.schwartz@gmail.com>
2018-07-16 14:37:38 -0400

    created new files and completed exercise4_1

--- exercise2.rb
+++ exercise2.rb
@@ -12,3 +12,8 @@ puts string
 #Part 3 of exercise 2
 total = 45628 * 7839
 puts "45628 multiplied by 7839 would be #{total}"
+
+#Part 4 of exercise 2
+#(true and false would be false) or (not(false) => evaluates to true)
+#would result in true since second statement would be true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
---
+++ exercise4_1.rb
@@ -0,0 +1,8 @@
+#This exercise compares a number to 100
+puts "Enter a number: "
+num = gets.to_i
+if num > 100
+  puts "That's a big number!"
+else
+  puts "Why not dream a little bigger?"
+end
